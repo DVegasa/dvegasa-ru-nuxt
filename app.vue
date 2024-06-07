46160b56c4569fcf60c663fc5a972a85a5823fbb
Ed Khalturin <ekhalturin@oggettoweb.com>
2024-06-07 21:11:01 +0300

    Фикс счётчика яндекс метрики

--- app.vue
+++ app.vue
@@ -1,10 +1,8 @@
 <template>
   <NuxtLayout name="layout-base">
-    <yandex-metrica-counter />
     <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-import YandexMetricaCounter from "~/modules/yandex-metrica/components/yandex-metrica-counter/YandexMetricaCounter.vue";
 </script>
--- modules/yandex-metrica/index.ts
+++ modules/yandex-metrica/index.ts
@@ -1,9 +1,11 @@
-import {addComponentsDir, createResolver, defineNuxtModule, extendPages} from "@nuxt/kit";
+import {addServerPlugin, createResolver, defineNuxtModule} from "@nuxt/kit";
 
 export default defineNuxtModule({
     meta: {
         name: 'yandex-metrica'
     },
     setup() {
+        const resolver = createResolver(import.meta.url)
+        addServerPlugin(resolver.resolve('./plugins/yandex-metrica-injection/yandex-metrica-injection.ts'))
     },
 })
