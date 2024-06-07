e23d1b76eb62c76f59bd229f308c188eb81875b2
Ed Khalturin <ekhalturin@oggettoweb.com>
2024-06-07 20:05:02 +0300

    Добавил счётчик яндекс метрики

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <NuxtLayout name="layout-base">
+    <yandex-metrica-counter />
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import YandexMetricaCounter from "~/modules/yandex-metrica/components/yandex-metrica-counter/YandexMetricaCounter.vue";
+</script>
---
+++ modules/yandex-metrica/index.ts
@@ -0,0 +1,9 @@
+import {addComponentsDir, createResolver, defineNuxtModule, extendPages} from "@nuxt/kit";
+
+export default defineNuxtModule({
+    meta: {
+        name: 'yandex-metrica'
+    },
+    setup() {
+    },
+})
