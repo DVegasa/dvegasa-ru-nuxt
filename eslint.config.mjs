32dfe8c2dc6ba1811e699be81b4bef6c83cb3b9b
Ed Khalturin <ekhalturin@oggettoweb.com>
2024-06-15 20:39:44 +0300

    Конфиг правил eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,19 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
+    'no-console': 'off',
+    'vue/no-v-html': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'vue/component-definition-name-casing': ['error', 'kebab-case'],
+    'vue/custom-event-name-casing': ['warn', 'kebab-case', {
+      // Допускаем двоеточия и тире "abc:def", "abc-def:ghi-jkl"
+      ignores: ['/^[a-z]+(?:-[a-z]+)*:[a-z]+(?:-[a-z]+)*$/u'],
+    }],
+    'vue/v-on-event-hyphenation': ['warn', 'always'],
+    'vue/attribute-hyphenation': ['warn', 'always'],
+    'indent': ['error', 2],
+
+    // TODO: Добавить правила сортировки импортов:
+    // 'simple-import-sort/imports'
   },
 })
