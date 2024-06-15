4148cc6b33693a241eaeb90aaef8db3fda911380
Ed Khalturin <ekhalturin@oggettoweb.com>
2024-06-15 11:14:14 +0300

    Стёр i18n костыли, начинаю заново

--- modules/i18n/i18n.config.ts
+++ modules/i18n/i18n.config.ts
@@ -1,22 +1,19 @@
 import {landing_ru} from "~/modules/landing/i18n/ru";
 import {landing_en} from "~/modules/landing/i18n/en";
-import {defineDefaultLocale} from "~/modules/i18n/utils/defineDefaultLocale";
 
-export default defineI18nConfig(() => ({
-    legacy: false,
-    locale: defineDefaultLocale(),
-    fallbackLocale: 'ru',
-    detectBrowserLanguage: {
-        useCookie: true,
-        cookieCrossOrigin: true,
-        cookieKey: 'i18n_redirected'
-    },
-    messages: {
-        ru: {
-            landing: {...landing_ru},
-        },
-        en: {
-            landing: {...landing_en},
+export default defineI18nConfig(() => {
+    return ({
+        strategy: 'no_prefix',
+        legacy: false,
+        defaultLocale: 'ru',
+        fallbackLocale: 'ru',
+        messages: {
+            ru: {
+                landing: {...landing_ru},
+            },
+            en: {
+                landing: {...landing_en},
+            }
         }
-    }
-}))
+    })
+})
--- modules/i18n/index.ts
+++ modules/i18n/index.ts
@@ -1,11 +1,9 @@
-import {addServerPlugin, createResolver, defineNuxtModule} from "@nuxt/kit";
+import {defineNuxtModule} from "@nuxt/kit";
 
 export default defineNuxtModule({
     meta: {
         name: 'i18n'
     },
     setup() {
-        // const resolver = createResolver(import.meta.url)
-        // addServerPlugin(resolver.resolve('./plugins/define-default-language/define-default-language.ts'))
     },
 })
